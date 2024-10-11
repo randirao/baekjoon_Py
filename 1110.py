e89d55c24f7b21e85a34b1136e02893e5380ed06
Han2 <randirao4245@gmail.com>
2024-10-11 22:04:02 +0900

    1110

--- 1110.py
+++ 1110.py
@@ -1,12 +1,15 @@
 n = int(input())  #26
-c = n
+c = n #c=26
 cnt=0
 
 while True :
-    a = n%10    #a=6
-    b = n//10+a   #b=2+6=8
+    #n=26 / n=68
+    a = n%10    #a=6 / a=8
+    b = n//10 + a   #b=2+6=8 / b=6+8=14
+    if b>9 :
+        b %= 10
     n = a*10 + b   #n=60+8=68
-    cnt+=1
+    cnt+=1  #cnt=1
     if n==c :
         break
 print(cnt)
