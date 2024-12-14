ed050d617275203e187b11e2f7be48a5a6cce087
Han2 <randirao4245@gmail.com>
2024-12-14 17:45:20 +0900

    10812

---
+++ 10812.py
@@ -0,0 +1,26 @@
+aX = [0, 0, -1, 1]
+aY = [-1, 1, 0, 0]
+
+def f(i, j) :
+    arr[i][j] = 0
+    for k in range(4) :
+        nX = i + aX[k]
+        nY = j + aY[k]
+        if (0 <= nX < n and 0 <= nY < m) and arr[nX][nY] == 1 :
+            f(nX, nY)
+
+t = int(input())
+for _ in range(t) :
+    m, n, k = map(int, input().split())
+    arr = [[0] * m for _ in range(n)]
+    for _ in range(k) :
+        x, y = map(int, input().split())
+        arr[y][x] = 1
+
+    count = 0
+    for i in range(n) :
+        for j in range(m) :
+            if arr[i][j] == 1 :
+                f(i, j)
+                count += 1
+    print(count)
