ea38d6f4d0f8d48fad93e1ab3a10e4a1bffe0546
Han2 <randirao4245@gmail.com>
2024-12-14 17:04:28 +0900

    1004

--- 1004.py
+++ 1004.py
@@ -1,21 +1,12 @@
 t = int(input())
 for _ in range(t):
+    count = 0
     x1, y1, x2, y2 = map(int, input().split())
     n = int(input())
     for i in range(n) :
         cx, cy, r = map(int, input().split())
-
-# 2 -> t
-# -5 1 12 1 -> x1, y1, x2, y2
-# 7 -> n
-# 1 1 8 -> cx, cy, r
-# -3 -1 1 -> cx, cy, r
-# 2 2 2 -> cx, cy, r
-# 5 5 1 -> cx, cy, r
-# -4 5 1 -> cx, cy, r
-# 12 1 1 -> cx, cy, r
-# 12 1 2 -> cx, cy, r
-
-# -5 1 5 1 -> x1, y1, x2, y2
-# 1 -> n
-# 0 0 2 -> cx, cy, r
+        if (cx - x1)**2 + (cy - y1)**2 <= r**2 or (cx - x2)**2 + (cy - y2)**2 <= r**2:
+            if(cx - x1)**2 + (cy - y1)**2 <= r**2 and (cx - x2)**2 + (cy - y2)**2 <= r**2 :
+                continue
+            count += 1
+    print(count)
