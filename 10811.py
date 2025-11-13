3bd8b7452e7e49be89d85483109ceba34efe03b2
randirao <randirao4245@gmail.com>
2025-11-14 00:19:22 +0900

    10811

--- 10811.py
+++ 10811.py
@@ -1,18 +1,10 @@
 n, m = map(int, input().split())
-arr = [i for i in range(1, n+1)] #초기화
-tmp = 0
-for _ in range(m) : #m번 돌리기
-    i, j = map(int, input().split()) #i, j입력
-    for k in range(j-1, i-1, -1): #i부터 j까지 돌리기
-        print(f"k: {k}")
-        tmp = arr[k]
-        arr[k] = arr[k-1]
-        arr[k-1] = tmp
-    print(*arr)
+lst = [idx for idx in range(1, n+1)]
 
-
-# 1 2 3 4 5
-# 1 2 3 5 4
-# 1 2 5 3 4
-# 1 5 2 3 4
-# 5 1 2 3 4
+for n in range(m):
+    i, j = map(int, input().split())
+    if i == j:
+        continue
+    else:
+        lst[i-1:j] = lst[i-1:j][::-1]
+print(*lst)
